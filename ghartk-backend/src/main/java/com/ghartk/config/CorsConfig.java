147323a3e97e86710669fd6daca7c9bbc572d1c7
Rohit Singh <64071423+rohitsingh2480@users.noreply.github.com>
2026-06-12 18:04:01 +0530

    Update CorsConfig.java

--- ghartk-backend/src/main/java/com/ghartk/config/CorsConfig.java
+++ ghartk-backend/src/main/java/com/ghartk/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.ghartk.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
