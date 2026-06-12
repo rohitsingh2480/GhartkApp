141af9b91009d6f1221098d863895fe9cdf60599
Rohit Singh <64071423+rohitsingh2480@users.noreply.github.com>
2026-06-12 18:08:14 +0530

    Update RefreshTokenRepository.java

--- ghartk-backend/src/main/java/com/ghartk/repository/RefreshTokenRepository.java
+++ ghartk-backend/src/main/java/com/ghartk/repository/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package com.ghartk.repository;
 
+
 import com.ghartk.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
