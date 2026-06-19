c2abc16d4726bb62b1889b7d384fd3f64ed96a34
Rohit Singh <64071423+rohitsingh2480@users.noreply.github.com>
2026-06-19 19:39:42 +0530

    Update CartItemRepository.java

--- ghartk-backend/src/main/java/com/ghartk/repository/CartItemRepository.java
+++ ghartk-backend/src/main/java/com/ghartk/repository/CartItemRepository.java
@@ -1,5 +1,6 @@
 package com.ghartk.repository;
 
+
 import com.ghartk.entity.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
