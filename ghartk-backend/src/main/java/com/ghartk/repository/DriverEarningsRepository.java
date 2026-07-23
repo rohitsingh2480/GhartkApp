3acf1777a3354196c606216942be3c8934331415
Rohit Singh <64071423+rohitsingh2480@users.noreply.github.com>
2026-07-23 18:56:08 +0530

    Add lines DriverEarningsRepository.java

--- ghartk-backend/src/main/java/com/ghartk/repository/DriverEarningsRepository.java
+++ ghartk-backend/src/main/java/com/ghartk/repository/DriverEarningsRepository.java
@@ -1,5 +1,6 @@
 package com.ghartk.repository;
 
+
 import com.ghartk.entity.DriverEarnings;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
