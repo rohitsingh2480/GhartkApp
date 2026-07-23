cb3dda12f98d66fbc59066d09a1fd68e83e50a0d
Rohit Singh <64071423+rohitsingh2480@users.noreply.github.com>
2026-07-23 18:53:07 +0530

    Add lines to DriverRepository.java

--- ghartk-backend/src/main/java/com/ghartk/repository/DriverRepository.java
+++ ghartk-backend/src/main/java/com/ghartk/repository/DriverRepository.java
@@ -1,5 +1,6 @@
 package com.ghartk.repository;
 
+
 import com.ghartk.entity.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
